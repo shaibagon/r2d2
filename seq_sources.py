a10585c9a8687f6a1dae16f39965e2e51e242e40
shaibagon <shaibagon@gmail.com>
2017-01-13 14:44:04 +0200

    s

--- seq_sources.py
+++ seq_sources.py
@@ -48,8 +48,6 @@ class msr_vtt_textual_descriptions(object):
         SEQ_GLOBALS.DIM = len(SEQ_GLOBALS.CHARS)
         SEQ_GLOBALS.CHAR_MAP = {c:i for i,c in enumerate(SEQ_GLOBALS.CHARS)}
         SEQ_GLOBALS.seqIterator = msr_vtt_textual_descriptions.seqIterator
-        SEQ_GLOBALS.singleSequenceProvider = msr_vtt_textual_descriptions.singleSequenceProvider
-
 
     class seqIterator(object):
         def __init__(self, phase):
@@ -84,47 +82,6 @@ class msr_vtt_textual_descriptions(object):
             s = self.sentences[self.si]
             self.si += 1
             return s
-
-    class singleSequenceProvider(Thread):
-        """
-        get sequences from inQ and output chunks to sequenceQ
-        """
-
-        def __init__(self, inQ, outQ, seq_len):
-            Thread.__init__(self)
-            self.inQ = inQ
-            self.outQ = outQ
-            self.seq_len = seq_len
-            self.daemon = True
-            self.start()
-
-        def run(self):
-            to_data = []
-            to_label = []
-            while True:
-                try:
-                    while len(to_data) < self.seq_len:
-                        s = self.inQ.get()
-                        l = [SEQ_GLOBALS.CHAR_MAP.get(c, SEQ_GLOBALS.UNK) for c in s]
-                        self.inQ.task_done()
-                        to_data.append(SEQ_GLOBALS.BOS)
-                        to_data += l
-                        to_label += l
-                        to_label.append(SEQ_GLOBALS.EOS)
-                    # we have enough data to push to batch
-                    while len(to_data) >= self.seq_len:
-                        data = np.zeros((self.seq_len, 1, SEQ_GLOBALS.DIM), dtype='f4')
-                        data[range(self.seq_len), 0, to_data[:self.seq_len]] = 1.0
-                        assert data.sum() == self.seq_len, "wrong hot vectors {} ones for {} rows".format(data.sum(),
-                                                                                                          self.seq_len)
-                        label = np.array(to_label[:self.seq_len])[:, None]
-                        self.outQ.put((data.copy(), label.copy()))
-                        del to_data[:self.seq_len]
-                        del to_label[:self.seq_len]
-                except Exception as e:
-                    glog.error('singleSequenceProvider: got error ({}): {}\n{}'.format(type(e).__name__, e,
-                                                                                       traceback.format_exc()))
-
 #------------------------------------------------------------------------------------------------------
 # class providing sequences taken from sir Arthur Conan Doyle novels.
 # see: https://www.gutenberg.org/wiki/Detective_Fiction_(Bookshelf)#Novels
@@ -133,41 +90,54 @@ class acd_novels(object):
     def __init__(self):
         global SEQ_GLOBALS
         # attributes
-        SEQ_GLOBALS.CHARS = '.abcdefghijklmnopqrstuvwxyz0123456789 U'
+        SEQ_GLOBALS.CHARS = self.pre_process()
         SEQ_GLOBALS.EOS = 0
         SEQ_GLOBALS.BOS = 0
         SEQ_GLOBALS.UNK = len(SEQ_GLOBALS.CHARS)-1
         SEQ_GLOBALS.DIM = len(SEQ_GLOBALS.CHARS)
         SEQ_GLOBALS.CHAR_MAP = {c:i for i,c in enumerate(SEQ_GLOBALS.CHARS)}
-        SEQ_GLOBALS.seqIterator = msr_vtt_textual_descriptions.seqIterator
-        SEQ_GLOBALS.singleSequenceProvider = msr_vtt_textual_descriptions.singleSequenceProvider
+        SEQ_GLOBALS.seqIterator = acd_novels.seqIterator
 
         # books
         self.books = ['244.txt', '1661.txt', '2097.txt', '221-0.txt', '2343.txt', '2344.txt', '2345.txt', '2346.txt',
                       '2347.txt', '2348.txt', '2349.txt', '2350.txt', '2852.txt', '3289-0.txt', '834-0.txt']
 
     def pre_process(self):
-
-        pass
+        wd = os.path.join(base_dir, 'acd')
+        if not os.path.isfile(os.path.join(wd,'train.txt')) or not os.path.isfile(os.path.join(wd,'test.txt')):
+            for bk in ['244.txt', '1661.txt', '2097.txt', '221-0.txt', '2343.txt', '2344.txt', '2345.txt', '2346.txt',
+                          '2347.txt', '2348.txt', '2349.txt', '2350.txt', '2852.txt', '3289-0.txt', '834-0.txt']:
+                with open(os.path.join(wd, bk), 'r') as R:
+                    b = R.read()
+                # remove headers and footers
+                spl = b.split('***')[2:-6]
+                b = '\n\n'.join(spl)
+                target = 'test.txt' if bk == '2345.txt' else 'train.txt'
+                with open(os.path.join(dw,target), 'a+') as W:
+                    W.write('{}\n\n'.format(b))
+        # read from cache
+        with open(os.path.join(wd,'train.txt'), 'r') as R:
+            cahrs = '|'+ sorted(set(R.read())) + '|'
+        return chars
 
     class seqIterator(object):
         def __init__(self, phase):
             if phase.lower() == 'train':
-                in_file_name = 'train_val_videodatainfo.json'
-                self.description = 'msr_vtt_textual_descriptions train'
+                in_file_name = os.path.join('acd', 'train.txt')
+                self.description = 'Sir ACD train'
             elif phase.lower() == 'test':
-                in_file_name = 'test_videodatainfo.json'
-                self.description = 'msr_vtt_textual_descriptions test'
+                in_file_name = os.path.join('acd', 'train.txt')
+                self.description = 'Sir ACD test'
             # read once
             with open(os.path.join(base_dir, in_file_name), 'r') as R:
-                inp = json.loads(R.read())
-            self.sentences = [s['caption'].lower() for s in inp['sentences']]
-            random.shuffle(self.sentences)
+                inp = R.read()
+            self.paragraphs = inp.split('\n\n')
+            random.shuffle(self.paragraphs)
             self.print_stats()
             self.si = 0
 
         def print_stats(self):
-            sl = np.array([len(s) for s in self.sentences])
+            sl = np.array([len(s) for s in self.paragraphs])
             glog.info(
                 '\nDataset stats for {}: {} seq with [{}..{}] <{}>_2, <{}>_1 chars/seq\n'.format(self.description, sl.size,
                                                                                                  sl.min(), sl.max(),
@@ -176,53 +146,13 @@ class acd_novels(object):
             return self
 
         def next(self):
-            if self.si >= len(self.sentences):
+            if self.si >= len(self.paragraphs):
                 glog.info('[{}] done epoch. reshuffling'.format(self.description))
-                random.shuffle(self.sentences)
+                random.shuffle(self.paragraphs)
                 self.si = 0
-            s = self.sentences[self.si]
+            s = self.paragraphs[self.si]
             self.si += 1
             return s
-
-    class singleSequenceProvider(Thread):
-        """
-        get sequences from inQ and output chunks to sequenceQ
-        """
-
-        def __init__(self, inQ, outQ, seq_len):
-            Thread.__init__(self)
-            self.inQ = inQ
-            self.outQ = outQ
-            self.seq_len = seq_len
-            self.daemon = True
-            self.start()
-
-        def run(self):
-            to_data = []
-            to_label = []
-            while True:
-                try:
-                    while len(to_data) < self.seq_len:
-                        s = self.inQ.get()
-                        l = [SEQ_GLOBALS.CHAR_MAP.get(c, SEQ_GLOBALS.UNK) for c in s]
-                        self.inQ.task_done()
-                        to_data.append(SEQ_GLOBALS.BOS)
-                        to_data += l
-                        to_label += l
-                        to_label.append(SEQ_GLOBALS.EOS)
-                    # we have enough data to push to batch
-                    while len(to_data) >= self.seq_len:
-                        data = np.zeros((self.seq_len, 1, SEQ_GLOBALS.DIM), dtype='f4')
-                        data[range(self.seq_len), 0, to_data[:self.seq_len]] = 1.0
-                        assert data.sum() == self.seq_len, "wrong hot vectors {} ones for {} rows".format(data.sum(),
-                                                                                                          self.seq_len)
-                        label = np.array(to_label[:self.seq_len])[:, None]
-                        self.outQ.put((data.copy(), label.copy()))
-                        del to_data[:self.seq_len]
-                        del to_label[:self.seq_len]
-                except Exception as e:
-                    glog.error('singleSequenceProvider: got error ({}): {}\n{}'.format(type(e).__name__, e,
-                                                                                       traceback.format_exc()))
 
 #------------------------------------------------------------------------------------------------------
 def get_configuration(config='msr-vtt-v0'):
