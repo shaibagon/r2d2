ed85c64a45fcd9dd9c9d5bc20f61138e383f8145
Shai Bagon <shai@magisto.com>
2017-01-17 17:47:44 +0200

    s

--- seq_sources.py
+++ seq_sources.py
@@ -193,13 +193,13 @@ def get_configuration(config='msr-vtt-v0'):
             variant = 'v0'
         configuration = {
             'variant': variant,
-            'input_params': {'train': {'seq_len': 300, 'batch_size': 10},
-                             'test': {'seq_len': 50, 'batch_size': 5}},
-            'layer_dims': [300, 300, 300, 300, 300, 300, SEQ_GLOBALS.DIM],
+            'input_params': {'train': {'seq_len': 300, 'batch_size': 14},
+                             'test': {'seq_len': 50, 'batch_size': 1}},
+            'layer_dims': [100, 100, 100, 100, 100, 100, SEQ_GLOBALS.DIM],
             'base_dir': base_dir,
             'test_niter': 5000,  # number of test iterations
-            'test_interval': 10000,  # when to snap and test
-            'train_niter': 10000000,  # number of train iterations
+            'test_interval': 5000,  # when to snap and test
+            'train_niter': 100000,  # number of train iterations
             'base_lr': 0.01,
             'debug': False
         }
